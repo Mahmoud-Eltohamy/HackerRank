8b5f8922ba88d21a1ada2a6d03313c9427589229
Mahmoud-Eltohamy <mahmoud.mohammed.elhady@gmail.com>
2024-09-18 11:13:48 +0300

    diagonalDifference

--- src/comparetriplets.py
+++ src/comparetriplets.py
@@ -1,20 +1,3 @@
-# problem statement
-# Alice and Bob each created one problem for HackerRank. A reviewer rates the two challenges, awarding points on a scale from 1 to 100 for three categories: problem clarity, originality, and difficulty.
-# The rating for Alice's challenge is the triplet a = (a[0], a[1], a[2]), and the rating for Bob's challenge is the triplet b = (b[0], b[1], b[2]).
-# The task is to find their comparison points by comparing a[0] with b[0], a[1] with b[1], and a[2] with b[2].
-# If a[i] > b[i], then Alice is awarded 1 point.
-# If a[i] < b[i], then Bob is awarded 1 point.
-# If a[i] = b[i], then neither person receives a point. Comparison points are the total points a person earned.
-# Given a and b, determine their respective comparison points.
-
-# solution
-# After reading the problem, we can observe 2 things:
-
-# The comparison array must come with Alice score first.
-# If both ratings are equal, nobody receives a point.
-
-
-
 def compareTriplets(a, b):
     '''
     Compares two lists of ratings and returns a list of two comparison points.
--- tests/test_comparetriplets.py
+++ tests/test_comparetriplets.py
@@ -1,8 +1,8 @@
 import pytest
-from src.comparetriplets import compareTriplets
+from src.compareTriplets import compareTriplets
 
 @pytest.mark.parametrize("a, b, expected", [
-    ([1, 2, 3], [3, 2, 1], [0, 0]),
+    ([1, 2, 3], [3, 2, 1], [1, 1]),
     ([1, 2, 3], [4, 5, 6], [0, 3]),
     ([1, 2, 3], [0, 0, 0], [3, 0]),
     ([5, 6, 7], [3, 6, 10], [1, 1])
